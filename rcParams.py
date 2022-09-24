78cb6106c5ba2a95ff6ea0e91e27064792a668c8
Jie Li <lijiefj@126.com>
2022-09-24 17:31:58 -0400

    Add rcParams

---
+++ rcParams.py
@@ -0,0 +1,41 @@
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib import cycler
+from matplotlib.ticker import FuncFormatter
+
+plt.style.use('seaborn-whitegrid')
+
+mpl.rcParams['figure.titlesize'] = 12
+
+mpl.rcParams['axes.titlesize'] = 10
+mpl.rcParams['axes.labelsize'] = 9
+mpl.rcParams['axes.labelcolor'] = 'gray'
+
+mpl.rcParams['axes.linewidth'] = 1.5
+mpl.rcParams['axes.spines.top'] = False
+mpl.rcParams['axes.spines.right'] = False
+mpl.rcParams['axes.spines.left'] = False
+
+mpl.rcParams['grid.linestyle'] = ':'
+mpl.rcParams['grid.linewidth'] = 0.4
+mpl.rcParams['grid.color'] = 'gray'
+
+mpl.rcParams['xtick.labelcolor'] = 'gray'
+mpl.rcParams['xtick.labelsize'] = 8.5
+mpl.rcParams['ytick.labelcolor'] = 'gray'
+mpl.rcParams['ytick.labelsize'] = 8.5
+
+mpl.rcParams['lines.linewidth'] = 0.6
+
+mpl.rcParams['axes.prop_cycle'] = cycler(color = [palette_hero, palette_primary, palette_hero_faded, palette_water])
+mpl.rcParams['figure.subplot.hspace'] = 0.3
+mpl.rcParams['figure.subplot.wspace'] = 0.1
+
+def thousands(x, pos):
+    return "{:1.0f}K".format(x*1e-3)
+
+def millions(x, pos):
+    return "{:1.1f}M".format(x*1e-6)
+
+formatter_thousands = FuncFormatter(thousands) # Number formatting for tick labels
+formatter_millions = FuncFormatter(millions)
